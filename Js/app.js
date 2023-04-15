4a3c1ee6649989c3d83b9ca0c6bfcc27a5916d62
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@gmail.com>
2023-04-15 03:37:12 +0200

    Task 01 is Done "Submit a Video request"

---
+++ Js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const formVidReq = document.getElementById("formVideoRequest");
+
+  formVidReq.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(formVidReq);
+
+    fetch('http://localhost:7777/video-request', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((bold) => bold.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+});
